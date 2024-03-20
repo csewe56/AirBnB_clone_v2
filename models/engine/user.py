c77e1439a5c5bd30ff4410bdd9b5be7867454315
csewe56 <csewe56@gmail.com>
2024-03-20 03:51:54 -0700

    Updating the User BaseModel and Base

---
+++ models/engine/user.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+'''This module updates user and inherits from BaseModel and Base'''
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from os import getenv
+
+storage_type = getenv("HBNB_TYPE_STORAGE")
+
+
+class User(BaseModel, Base):
+    '''This class represents the user by various attributes'''
+    __tablename__ = 'users'
+    if storage_type == 'db':
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        places = relationship('Place', cascade="all,delete", backref="user")
+        reviews = relationship('Review', cascade="all,delete", backref="user")
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
